fc29bdd2b062200a773e7d77101dc694776a04fa
Junkyu Park <junk3843@naver.com>
2017-03-27 10:59:16 +0900

    Add nthToLast methods in sinle linked list

--- linkedlist/single/Node.java
+++ linkedlist/single/Node.java
@@ -62,5 +62,53 @@ public class Node<T> {
 
         return head;
     }
+
+    public static Node nthToLastNonRecursive(Node head, int index){
+        /*Non Recursive*/
+        Node curNode = head;
+        int length = 0;
+        int valueIndex = 0;
+
+        while(curNode != null){
+            length++;
+            curNode = curNode.getNext();
+        }
+
+        curNode = head;
+        valueIndex = length - 1 - index;
+        for(int i = 0 ; i < valueIndex; i++ ){
+            curNode = curNode.getNext();
+        }
+
+        return curNode;
+
+    }
+
+    public static Node nthToLastRecursive(Node head, int index, IntegerWrapper indexVar){
+        /* Recursive */
+        if(head.getNext() == null){
+            return null;
+        }
+        Node node = nthToLastRecursive(head.getNext(), index, indexVar);
+        indexVar.setCount(indexVar.getCount() + 1);
+        if(indexVar.getCount() == index){
+            return head;
+        }
+
+        return node;
+    }
+}
+class IntegerWrapper{
+    private int count;
+
+    public IntegerWrapper(){
+        this.count = 0;
+    }
+    public void setCount(int count){
+        this.count = count;
+    }
+    public int getCount(){
+        return this.count;
+    }
 }
 
